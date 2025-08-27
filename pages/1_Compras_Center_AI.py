59f4d2f72fe71c2a2eddfe0e36db40158574a7fa
Lourival Pinheiro <lourivaltpneto@outlook.com>
2025-08-27 00:52:34 -0300

    final try

--- pages/1_Compras_Center_AI.py
+++ pages/1_Compras_Center_AI.py
@@ -12,7 +12,7 @@ def carrega_modelo():
     st.session_state['chat'] = chat
 
 def chat_page():
-    HeaderMenu.hide_menu_ai_menu()
+    HeaderMenu.hide_menu()
     st.header(":material/robot_2: COMPRAS CENTER AI", divider=True)
     if st.button("Inicializar IA", use_container_width=True):
             carrega_modelo()
